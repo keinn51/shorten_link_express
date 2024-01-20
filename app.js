e366bc37cf024e016094b5c9c49333b8877ceffa
keinn51 <soogood97@gmail.com>
2024-01-20 14:53:09 +0900

    [feat] html 페이지를 만들어 인풋과 버튼 추가해두기

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 require("dotenv").config();
 const mysql = require("mysql");
+const path = require("path");
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -11,6 +12,10 @@ const connection = mysql.createConnection({
   port: process.env.MYSQL_PORT,
   database: process.env.MYSQL_DATABASE,
 });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 function getAllLinks(callback) {
   connection.query(`SELECT * FROM shortenlink.link;`, (err, rows, fields) => {
@@ -21,7 +26,8 @@ function getAllLinks(callback) {
 
 // 루트 경로에 대한 핸들러
 app.get("/", (req, res) => {
-  res.send("안녕하세요, Express 서버입니다!");
+  // res.send("안녕하세요, Express 서버입니다!");
+  res.render("index.ejs");
 });
 
 // 서버 시작
@@ -29,7 +35,7 @@ app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
 
-app.get("/1", function (req, res, next) {
+app.get("/test", function (req, res, next) {
   getAllLinks((rows) => {
     console.log(rows);
   });
